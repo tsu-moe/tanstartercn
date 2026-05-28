a4698bd9386aa2ad6522f59c87a572334b5835d1
Aniket-508 <pawaraniket508@gmail.com>
2026-05-29 03:16:24 +0530

    chore: remove Sponsor link from site header

--- components/site-header.tsx
+++ components/site-header.tsx
@@ -17,7 +17,6 @@ import { source } from "@/lib/source";
 const navItems = [
   { href: ROUTES.DOCS, label: "Docs" },
   { href: ROUTES.DOCS_COMPONENTS, label: "Components" },
-  { href: ROUTES.SPONSOR, label: "Sponsor" },
 ];
 
 export const SiteHeader = () => (
